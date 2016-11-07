94b322b37d1eaf772027adb2051a591aefc9ea83
Alexander Shek <sheacspear@gmail.com>
2016-11-07 03:00:00 +0300

    prepare

--- report/src/main/java/ru/zorb/services/report/ReportService.java
+++ report/src/main/java/ru/zorb/services/report/ReportService.java
@@ -1,28 +1,22 @@
 package ru.zorb.services.report;
 
-import org.jooq.Record;
 import org.springframework.stereotype.Component;
-
-import ru.zorb.services.meta.ContentDataSource;
 import ru.zorb.services.report.impl.ReportImpl;
-import ru.zorb.services.report.impl.UniversalDynamicContentListReport;
 
 @Component
 public class ReportService implements IReportService {
 
-  //@Autowired
-  private ContentDataSource<Record> dataSource;
 
-	//@Autowired
-	//private ViewMapperFactory viewMapperFactory;
+    //@Autowired
+    //private ViewMapperFactory viewMapperFactory;
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see ru.zorb.services.report.IReportService#getReport(java.lang.String)
-	 */
-	@Override
-	public IReport getReport(String report) {
-		return new ReportImpl();
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see ru.zorb.services.report.IReportService#getReport(java.lang.String)
+     */
+    @Override
+    public IReport getReport(String report) {
+        return new ReportImpl();
+    }
 }
