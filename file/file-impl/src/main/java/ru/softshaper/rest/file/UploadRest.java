034934eb830af81f9ec45f11f1de2666ded9188f
Alexander Shek <sheacspear@gmail.com>
2017-05-22 03:00:00 +0300

    refactoring

--- file/file-impl/src/main/java/ru/softshaper/rest/file/UploadRest.java
+++ file/file-impl/src/main/java/ru/softshaper/rest/file/UploadRest.java
@@ -26,62 +26,47 @@ import java.util.List;
 @Path("/pr/upload")
 public class UploadRest {
   private static final Logger log = LoggerFactory.getLogger(UploadRest.class);
-	@Autowired
-	private FileObjectDataSource fileObjectDataSource;
+  @Autowired
+  private FileObjectDataSource fileObjectDataSource;
 
-	/**
-	 * inject this from spring context
-	 */
-	@PostConstruct
-	public void init() {
-		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
-	}
+  /**
+   * inject this from spring context
+   */
+  @PostConstruct
+  public void init() {
+    SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+  }
 
-	/**
-	 * @param attachments
-	 * @param request
-	 * @return
-	 */
-	@POST
-	@Path("/uploadFile")
-	@Consumes("multipart/form-data")
-	public String uploadFile(List<Attachment> attachments) {
-		for (Attachment attachment : attachments) {
-			DataHandler handler = attachment.getDataHandler();
-			InputStream stream = null;
-			try {
-				stream = handler.getInputStream();
-				MultivaluedMap<String, String> map = attachment.getHeaders();
-				FileObjectBean fileObject = new FileObjectBean(null, getFileName(map), null, null, null);
-				return fileObjectDataSource.createFile(fileObject, stream);
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				if (stream != null) {
-					try {
-						stream.close();
-					} catch (IOException e) {
-						log.error(e.getMessage(),e);
-					}
-				}
-			}
-		}
-		return null;
-	}
+  /**
+   * @param attachments
+   * @param request
+   * @return
+   */
+  @POST
+  @Path("/uploadFile")
+  @Consumes("multipart/form-data;charset=UTF-8")
+  public String uploadFile(List<Attachment> attachments) {
+    for (Attachment attachment : attachments) {
+      DataHandler handler = attachment.getDataHandler();
+      String filename = handler.getName();
+      InputStream stream = null;
+      try {
+        stream = handler.getInputStream();
+        FileObjectBean fileObject = new FileObjectBean(null, filename, null, null, null);
+        return fileObjectDataSource.createFile(fileObject, stream);
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        if (stream != null) {
+          try {
+            stream.close();
+          } catch (IOException e) {
+            log.error(e.getMessage(), e);
+          }
+        }
+      }
+    }
+    return null;
+  }
 
-	/**
-	 * @param header
-	 * @return
-	 */
-	private String getFileName(MultivaluedMap<String, String> header) {
-		String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
-		for (String filename : contentDisposition) {
-			if ((filename.trim().startsWith("filename"))) {
-				String[] name = filename.split("=");
-				String exactFileName = name[1].trim().replaceAll("\"", "");
-				return exactFileName;
-			}
-		}
-		return "unknown";
-	}
 }
