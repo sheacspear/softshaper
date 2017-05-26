b85de47076709b03c165003bad7daa9eb5248e77
Alexander Rostov <arostov83@gmail.com>
2017-05-26 03:00:00 +0300

    Отправка события о создании документа

--- rules/src/main/java/ru/softshaper/services/drools/saver/Saver.java
+++ rules/src/main/java/ru/softshaper/services/drools/saver/Saver.java
@@ -1,25 +1,41 @@
 package ru.softshaper.services.drools.saver;
 
-import java.io.ByteArrayInputStream;
-import java.sql.Date;
-import java.util.List;
-
+import com.google.common.eventbus.EventBus;
 import org.jooq.tools.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import ru.softshaper.bean.file.FileObjectBean;
+import ru.softshaper.datasource.events.ObjectCreated;
 import ru.softshaper.datasource.file.FileObjectDataSource;
 import ru.softshaper.services.drools.bean.Data;
 import ru.softshaper.services.drools.bean.Doc;
 import ru.softshaper.services.drools.bean.MetaData;
+import ru.softshaper.services.meta.MetaClass;
+import ru.softshaper.services.meta.MetaField;
+import ru.softshaper.services.meta.MetaStorage;
+import ru.softshaper.services.security.ContentSecurityManager;
 import ru.softshaper.storage.jooq.tables.daos.DcDcProjectDao;
 import ru.softshaper.storage.jooq.tables.daos.DcDocDao;
 import ru.softshaper.storage.jooq.tables.pojos.DcDcProject;
 import ru.softshaper.storage.jooq.tables.pojos.DcDoc;
 
+import java.io.ByteArrayInputStream;
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Component
 public class Saver {
+
+  @Autowired
+  private ContentSecurityManager securityManager;
+
+  @Autowired
+  private EventBus eventBus;
+
+  @Autowired
+  private MetaStorage metaStorage;
 
   @Autowired
   private DcDocDao dcDocDao;
@@ -44,6 +60,17 @@ public class Saver {
     doc.setDate67(new Date(System.currentTimeMillis()));
     dcDocDao.insert(doc);
     String string = doc.getId() != null ? doc.getId().toString() : null;
+    MetaClass docClass = metaStorage.getMetaClass("doc");
+    if (docClass != null) {
+      Map<MetaField, Object> values = new HashMap<>();
+      values.put(docClass.getField("project"), doc.getProject());
+      values.put(docClass.getField("tags"), doc.getTags());
+      values.put(docClass.getField("description"), doc.getDescription());
+      values.put(docClass.getField("source"), doc.getSource());
+      values.put(docClass.getField("date67"), doc.getDate67());
+      ObjectCreated event = new ObjectCreated(docClass, string, values, securityManager.getCurrentUserLogin());
+      eventBus.post(event);
+    }
     return new Doc(string, data.getFileName());
   }
 }
